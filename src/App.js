fcb4b13014c36a9ae956387a643ffa3282244b13
Aakash <aakash.codes02@gmail.com>
2023-02-04 22:30:30 +0530

    created the react-app with state management using redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './actions/index';
+
 function App() {
+  const myState = useSelector(state => state.changeNumber);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="bg-slate-800 h-screen flex justify-center items-center flex-col">
       <h1 className="text-6xl text-slate-500 font-bold">
@@ -12,17 +19,20 @@ function App() {
         <button
           className="px-8 py-4 flex items-center justify-center text-blue-900 text-4xl font-bold hover:bg-slate-700 hover:text-slate-200 rounded"
           title="Decrement"
+          onClick={() => dispatch(decNumber())}
         >
           -
         </button>
         <input
           type="text"
           className="py-4 bg-slate-300 flex items-center justify-center rounded text-4xl font-semibold text-slate-700 w-fit max-w-[5rem] text-center border-none outline-none"
-          value="0"
+          value={myState}
+          readOnly
         />
         <button
           className="px-8 py-4 flex items-center justify-center text-blue-900 text-4xl font-bold hover:bg-slate-700 hover:text-slate-200 rounded"
           title="Increment"
+          onClick={() => dispatch(incNumber())}
         >
           +
         </button>
